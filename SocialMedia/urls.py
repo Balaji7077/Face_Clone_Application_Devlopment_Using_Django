74e3c59fd643e1cdf38997826aa8ea2e2e12edde
Balaji7077 <Balajisabat6@gmail.com>
2024-01-17 00:49:42 +0530

    first commit

--- SocialMedia/urls.py
+++ SocialMedia/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+import SocialApp
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('SocialApp.urls'))
+    path('SocialApp/',include('SocialApp.urls')),
 ]
